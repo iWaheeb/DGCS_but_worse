37c5c729eedf7095645e2bac26f1ce03ce68413f
iWaheeb <waheebbasalem@gmail.com>
2024-02-13 21:03:01 +0300

    changed connection ip/port

--- listen.py
+++ listen.py
@@ -1,11 +1,9 @@
 from pymavlink import mavutil
 
 # Start a connection listening on a UDP port
-the_connection = mavutil.mavlink_connection('udpin:localhost:14551')
+the_connection = mavutil.mavlink_connection('127.0.0.1:14559')
 
 # Wait for the first heartbeat 
 #   This sets the system and component ID of remote system for the link
 the_connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
-
-# Once connected, use 'the_connection' to get and send messages
+print(f"Heartbeat from system (system {the_connection.target_system} component {the_connection.target_component})")
